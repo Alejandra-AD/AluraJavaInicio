f62df49fd786ab4f3c0b2360590f58874fe33741
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-04-04 18:41:17 -0300

    Update changes

--- src/DesafioCalculoTemperatura.java
+++ src/DesafioCalculoTemperatura.java
@@ -5,9 +5,9 @@ public class DesafioCalculoTemperatura {
 
         /*Consejo: La fórmula para convertir temperaturas de grados Celsius a Fahrenheit es: (temperatura * 1.8) + 32.*/
 
-        System.out.println("Conversor de temperatura en grados Celsius a Fahrenheit.\nIngrese la temperatura en grados Celsius: ");
+        System.out.println("Conversor de temperatura en grados Celsius a Fahrenheit.\nIngrese la temperatura en grados Celsius (ejemplo: 7,6): ");
         Scanner scannerTemperaturaCelsius = new Scanner(System.in);
-        int temperaturaEnCelsius = scannerTemperaturaCelsius.nextInt();
+        double temperaturaEnCelsius = scannerTemperaturaCelsius.nextDouble();
         double resultadoFormula = temperaturaEnCelsius * 1.8 + 32;
 
         System.out.println( temperaturaEnCelsius+ " grados Celsius corresponden a " + resultadoFormula + " grados Fahrenheit.");
